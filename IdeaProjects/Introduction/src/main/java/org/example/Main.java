e87c85bf584bf5ece283bf8c35885c26fa941a1c
niccolosea0 <niccolosea@gmail.com>
2026-02-24 21:38:43 +0400

    Add WebDriver

--- IdeaProjects/Introduction/src/main/java/org/example/Main.java
+++ IdeaProjects/Introduction/src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Main {
     public static void main(String[] args) {
 
-
-
+        // Invoking Browser
+        // Firefox - FirefoxDriver -> Methods
+        // Declaring WebDriver type to have access only to WebdRiver methods, not specific ones for browsers
+        WebDriver driver = new FirefoxDriver();
     }
 }
