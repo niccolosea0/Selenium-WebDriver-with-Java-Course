2ef7f82e58fec8d028c4935db85613cdd4ce540d
niccolosea0 <niccolosea@gmail.com>
2026-02-25 20:03:22 +0400

    Add ChromeDriver

--- IdeaProjects/Introduction/src/main/java/org/example/Main.java
+++ IdeaProjects/Introduction/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
@@ -9,6 +11,7 @@ public class Main {
         // Invoking Browser
         // Firefox - FirefoxDriver -> Methods
         // Declaring WebDriver type to have access only to WebdRiver methods, not specific ones for browsers
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new ChromeDriver();
+
     }
 }
