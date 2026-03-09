24276a20602f1004078762f00801c12bd00725d7
niccolosea0 <niccolosea@gmail.com>
2026-03-09 21:21:21 +0400

    Adding cssSelector and implicitWait

--- IdeaProjects/Locators/src/main/java/org/example/Main.java
+++ IdeaProjects/Locators/src/main/java/org/example/Main.java
@@ -2,7 +2,10 @@ package org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,10 +13,15 @@ public class Main {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.findElement(By.id("inputUsername")).sendKeys("rahul");
         driver.findElement(By.name("inputPassword")).sendKeys("rahul123");
 
         driver.findElement(By.className("signInBtn")).click();
+
+        WebElement errorMessage = driver.findElement(By.cssSelector("p.error"));
+        System.out.println(errorMessage.getText());
+
 
         // driver.close();
 
