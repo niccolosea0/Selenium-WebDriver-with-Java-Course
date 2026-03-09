104c274678e1d2779f8c3d63a1cc0c3d8989e8fb
niccolosea0 <niccolosea@gmail.com>
2026-03-09 20:18:30 +0400

    finding elements, sending keys, clicking on button

--- IdeaProjects/Locators/src/main/java/org/example/Main.java
+++ IdeaProjects/Locators/src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.name("inputPassword")).sendKeys("rahul123");
+
+        driver.findElement(By.className("signInBtn")).click();
+
+        // driver.close();
+
     }
 }
