5dff48c8f9e8ebc0624c201ff0bc5e4d0b0b78fe
niccolosea0 <niccolosea@gmail.com>
2026-03-10 17:43:31 +0400

    add regular expressions selecting way

--- IdeaProjects/Locators/src/main/java/org/example/Main.java
+++ IdeaProjects/Locators/src/main/java/org/example/Main.java
@@ -8,26 +8,46 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
 
+        // Login
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.findElement(By.id("inputUsername")).sendKeys("rahul");
         driver.findElement(By.name("inputPassword")).sendKeys("rahul123");
-
         driver.findElement(By.className("signInBtn")).click();
-
         WebElement errorMessage = driver.findElement(By.cssSelector("p.error"));
-        System.out.println(errorMessage.getText());
 
+        System.out.println(errorMessage.getText());
+        // Click forgot password
         driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+
+        // Forgot password
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
         driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("John@random.com");
         driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).clear();
         driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("matteroftime@fly.com");
+        driver.findElement(By.xpath("//form/input[3]")).sendKeys("12344532");
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        System.out.println(driver.findElement(By.cssSelector("form p")).getText());
 
+        // Go to login
+        driver.findElement(By.cssSelector(".go-to-login-btn")).click();
+
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#inputUsername")).sendKeys("John");
+        driver.findElement(By.cssSelector("input[type*='pass'")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("chkboxOne")).click();
+        driver.findElement(By.cssSelector("input[name='chkboxTwo']")).click();
+        driver.findElement(By.xpath("//button[contains(@class, 'submit')]")).click();
+
+
+        // log out
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector(".logout-btn")).click();
 
         // driver.close();
 
