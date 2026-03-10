f8f0c538a5cef1bb0ab82ef0d3f81846e9a1ae0e
niccolosea0 <niccolosea@gmail.com>
2026-03-10 17:54:40 +0400

    deleted redunant exception throwing

--- IdeaProjects/Locators/src/main/java/org/example/Locators2.java
+++ IdeaProjects/Locators/src/main/java/org/example/Locators2.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class Locators2 {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
