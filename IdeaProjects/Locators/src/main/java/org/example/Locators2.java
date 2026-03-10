bd01120354dd6a129a040327d472573c88ab6c13
niccolosea0 <niccolosea@gmail.com>
2026-03-10 17:46:25 +0400

    Add another class for clarity

---
+++ IdeaProjects/Locators/src/main/java/org/example/Locators2.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Locators2 {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.cssSelector("#inputUsername")).sendKeys("John");
+        driver.findElement(By.cssSelector("input[type*='pass'")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("chkboxOne")).click();
+        driver.findElement(By.cssSelector("input[name='chkboxTwo']")).click();
+        driver.findElement(By.xpath("//button[contains(@class, 'submit')]")).click();
+
+    }
+}
