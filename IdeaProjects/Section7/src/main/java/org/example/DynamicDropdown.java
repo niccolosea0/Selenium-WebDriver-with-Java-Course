324c2ac9c57079c14e85c5f065eadc57e002adf9
niccolosea0 <niccolosea@gmail.com>
2026-03-11 17:42:45 +0400

    Changed number indexing to parent-child reference

--- IdeaProjects/Section7/src/main/java/org/example/DynamicDropdown.java
+++ IdeaProjects/Section7/src/main/java/org/example/DynamicDropdown.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DynamicDropdown {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
@@ -13,7 +13,7 @@ public class DynamicDropdown {
         driver.findElement(By.id("ctl00_mainContent_ddl_originStation1")).click();
         driver.findElement(By.xpath("//option[@value='BLR']")).click();
 
-        driver.findElement(By.xpath("(//option[@value='DEL'])[2]")).click();
+        driver.findElement(By.xpath("//select[@id='ctl00_mainContent_ddl_destinationStation1'] //option[@value='MAA'] ")).click();
 
     }
 }
