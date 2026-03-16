1473457d42a389145c34acb00594d36d3e46aee9
niccolosea0 <niccolosea@gmail.com>
2026-03-16 16:03:23 +0400

    Changed to FireFoxDriver

--- IdeaProjects/Section7/src/main/java/org/example/DynamicDropdown.java
+++ IdeaProjects/Section7/src/main/java/org/example/DynamicDropdown.java
@@ -3,11 +3,12 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DynamicDropdown {
     public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
         driver.findElement(By.id("ctl00_mainContent_ddl_originStation1")).click();
