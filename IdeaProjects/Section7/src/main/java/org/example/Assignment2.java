a7458ee192e7dafe2d8d7e0fcdcf9286d6750207
niccolosea0 <niccolosea@gmail.com>
2026-03-16 17:41:23 +0400

    Add Asssignemnt2 driver get

---
+++ IdeaProjects/Section7/src/main/java/org/example/Assignment2.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+// Please go through above video to understand the Assignment which need to be completed
+// Questions for this assignment
+
+public class Assignment2 {
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+
+
+    }
+}
