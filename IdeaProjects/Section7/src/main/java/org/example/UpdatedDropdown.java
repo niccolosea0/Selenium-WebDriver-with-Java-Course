d683f157486ddbe90cb70c2d7dd6529e155eedec
niccolosea0 <niccolosea@gmail.com>
2026-03-16 16:18:07 +0400

    Add selecting checkboxes

--- IdeaProjects/Section7/src/main/java/org/example/UpdatedDropdown.java
+++ IdeaProjects/Section7/src/main/java/org/example/UpdatedDropdown.java
@@ -2,13 +2,24 @@ package org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+
+import java.util.List;
 
 public class UpdatedDropdown {
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+        WebElement srCitizenChecBox = driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']"));
+        srCitizenChecBox.click();
+        System.out.println(srCitizenChecBox.isSelected());
+
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector("div[id='discount-checkbox'] input[type='checkbox']"));
+        System.out.println("Number of checkboxes: " + checkBoxes.size());
+
 
         driver.findElement(By.id("divpaxinfo")).click();
         Thread.sleep(1000);
