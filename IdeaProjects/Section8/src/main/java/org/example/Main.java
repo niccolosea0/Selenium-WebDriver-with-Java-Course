36931da01eb792686e3e98812d0a3f088a91bd79
niccolosea0 <niccolosea@gmail.com>
2026-03-30 16:57:33 +0400

    add Section8 first lecture

--- IdeaProjects/Section8/src/main/java/org/example/Main.java
+++ IdeaProjects/Section8/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+
+        List<WebElement> products =  driver.findElements(By.cssSelector("h4.product-name"));
+
+        String[] names = {"Cucumber", "Broccoli"};
+
+        for (int i = 0; i < products.size(); i++) {
+            String productName = products.get(i).getText();
+            if (productName.contains(names[0])) {
+                System.out.println(productName);
+                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+                break;
+            }
+        }
     }
 }
