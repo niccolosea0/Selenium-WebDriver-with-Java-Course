96c3973746039510ee996daa01551031cec637be
niccolosea0 <niccolosea@gmail.com>
2026-06-19 10:21:38 +0400

    Removed duration import

--- IdeaProjects/section10/src/main/java/org/example/FrameTest.java
+++ IdeaProjects/section10/src/main/java/org/example/FrameTest.java
@@ -10,7 +10,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.List;
 
 public class FrameTest {
