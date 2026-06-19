e512bebe5e0910090fc2bbf9125d8b95c2bb4484
niccolosea0 <niccolosea@gmail.com>
2026-06-19 18:38:39 +0400

    Add getLinksCount

--- IdeaProjects/section11/src/main/java/org/example/Main.java
+++ IdeaProjects/section11/src/main/java/org/example/Main.java
@@ -1,17 +1,36 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.List;
+
+public class Main {
+
+        // 1. Get count of links on the page
+        WebDriver driver;
+
+        @BeforeMethod
+        public void setup() {
+            driver = new FirefoxDriver();
+            driver.get("https://rahulshettyacademy.com/AutomationPractice/");
         }
-    }
+
+        @Test(priority = 1)
+        public void getLinksCount() {
+            List<WebElement> linksList = driver.findElements(By.tagName("a"));
+            System.out.println("Number of links: " + linksList.size());
+        }
+
+
+
+        @AfterMethod
+        public void tearNika() {
+            driver.quit();
+        }
 }
