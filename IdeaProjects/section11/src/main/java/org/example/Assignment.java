04fb337d8c8c5c0aad0539e253f155d58b7ddb4f
niccolosea0 <niccolosea@gmail.com>
2026-06-22 20:42:29 +0400

    Removed redundant import

--- IdeaProjects/section11/src/main/java/org/example/Assignment.java
+++ IdeaProjects/section11/src/main/java/org/example/Assignment.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
