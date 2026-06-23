d0827fb70d0f3051ca93cddf4f79935e98c4834b
niccolosea0 <niccolosea@gmail.com>
2026-06-23 11:48:51 +0400

    Add values test

--- IdeaProjects/section12/src/main/java/org/example/JavaScriptExecutorDemo.java
+++ IdeaProjects/section12/src/main/java/org/example/JavaScriptExecutorDemo.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class JavaScriptExecutorDemo {
 
@@ -14,8 +19,25 @@ public class JavaScriptExecutorDemo {
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
         js.executeScript("window.scrollBy(0, 500)");
-        Thread.sleep(3000);
         js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+
+        List<WebElement> values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+
+        int sum = 0;
+
+        for (int i = 0; i < values.size(); i++) {
+            int value = Integer.parseInt(values.get(i).getText());
+            sum += value;
+        }
+
+        System.out.println("Sum: " + sum);
+
+        String[] totalAmountText = driver.findElement(By.cssSelector(".totalAmount")).getText().split(":");
+        int totalAmount = Integer.parseInt(totalAmountText[1].trim());
+
+        Assert.assertEquals(sum, totalAmount);
+
+        driver.quit();
 
     }
 }
