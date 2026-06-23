821b7874e2f62623991838e46de483a4ee115e18
niccolosea0 <niccolosea@gmail.com>
2026-06-23 20:08:22 +0400

    Add setup for proxy and download directory

--- IdeaProjects/section13/src/main/java/org/example/SSLCheck.java
+++ IdeaProjects/section13/src/main/java/org/example/SSLCheck.java
@@ -1,13 +1,26 @@
 package org.example;
 
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SSLCheck {
     public static void main(String[] args) {
 
         FirefoxOptions options = new FirefoxOptions();
+        Proxy proxy = new Proxy();
+
+        // Setup Proxy
+        proxy.setHttpProxy("ipaddress:4444");
+        options.setCapability("proxy", proxy);
+
+        // Set download directory
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("download.default_directory", "/directory/path");
 
         // Proceed with insecure way
         options.setAcceptInsecureCerts(true);
