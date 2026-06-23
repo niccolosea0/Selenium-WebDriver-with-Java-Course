0733b9126bf30a625cefb501e1ab955e33dcbfb7
niccolosea0 <niccolosea@gmail.com>
2026-06-23 13:55:51 +0400

    Add Assignment8

---
+++ IdeaProjects/section12/src/main/java/org/example/SecondAssignment.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class SecondAssignment {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        driver = new FirefoxDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+    }
+
+    @Test
+    public void testAutoComplete() throws InterruptedException {
+
+        String country = "United States (USA)";
+        String threeChar = "Uni";
+
+        // Get autoCompleteInput field and send keys
+        WebElement autoCompleteInput = driver.findElement(By.id("autocomplete"));
+        autoCompleteInput.sendKeys(threeChar);
+
+        // Get list of AUto Suggestions
+        List<WebElement> autoCompleteSuggestions = hoverOverAutoSuggestions();
+
+        // Method that finds desired country, by pressing key down
+        findDesiredCountry(autoCompleteSuggestions, autoCompleteInput, country);
+
+        // Get input field value after auto-suggestion updates it
+        String inputValue = autoCompleteInput.getAttribute("value");
+
+        Assert.assertEquals(inputValue, country);
+    }
+
+    public List<WebElement> hoverOverAutoSuggestions() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        return wait.until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                        By.cssSelector("ul[id='ui-id-1'] li")
+                )
+        );
+    }
+
+    public void findDesiredCountry(List<WebElement> list, WebElement input, String country) {
+        for (WebElement suggestion : list) {
+            input.sendKeys(Keys.DOWN);
+            if (suggestion.getText().equals(country)) {
+                break;
+            }
+        }
+    }
+
+    @AfterClass
+    public void tearDown() {
+        //driver.quit();
+    }
+}
